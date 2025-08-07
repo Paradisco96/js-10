b1261c88cd16bac6392ec80c4a4be909c8586b24
Paradisco96 <mr.wolf.dsgn@gmail.com>
2025-08-07 08:25:49 +0300

    feat: add key-, mouse-, touchHandler

--- index.js
+++ index.js
@@ -1,36 +1,122 @@
-const slides = document.querySelectorAll('.slide');
-const pause = document.getElementById('pause');
+const container = document.querySelector("#carousel");
+const slidesContainer = container.querySelector("#slides-container");
+const slides = container.querySelectorAll(".slide");
+const indicatorContainer = container.querySelector("#indicators-container");
+const indicators = container.querySelectorAll(".indicator");
+const pauseButton = container.querySelector("#pause-btn");
+const nextButton = container.querySelector("#next-btn");
+const prevButton = container.querySelector("#prev-btn");
+
+const SLIDES_COUNT = slides.length;
+const CODE_ARROW_RIGHT = "ArrowRight";
+const CODE_ARROW_LEFT = "ArrowLeft";
+const CODE_SPACE = "Space";
+const FA_PAUSE = '<i class="fas fa-pause"></i>';
+const FA_PLAY = '<i class="fas fa-play"></i>';
+const TIMER_INTERVAL = 2000;
+const SWIPE_THERESHOLD = 100;
 
 let currentSlide = 0;
 let isPlaying = true;
+let timerId = null;
+let swipeStartX = null;
+let swipeEndX = null;
 
-function nextSlide() {
-  slides[currentSlide].classList.toggle('active');
-currentSlide = (currentSlide + 1) % slides.length;
-  slides[currentSlide].classList.toggle('active');
+function gotoNth(n) {
+  slides[currentSlide].classList.toggle("active");
+  indicators[currentSlide].classList.toggle("active");
+  currentSlide = (n + SLIDES_COUNT) % SLIDES_COUNT;
+  slides[currentSlide].classList.toggle("active");
+  indicators[currentSlide].classList.toggle("active");
 }
 
-let slideInterval = setInterval(nextSlide, 2000);
+function gotoNext() {
+  gotoNth(currentSlide + 1);
+}
 
-function playSlideshow() {
-  slideInterval = setInterval(nextSlide, 2000);
+function gotoPrev() {
+  gotoNth(currentSlide - 1);
+}
+
+function tick() {
+  timerId = setInterval(gotoNext, TIMER_INTERVAL);
+}
+
+function playHandler() {
+  pauseButton.innerHTML = FA_PAUSE;
   isPlaying = true;
-  pause.innerHTML = 'Pause';
+  tick();
 }
 
-function pauseSlideshow() {
-isPlaying = false;
-pause.innerHTML = 'Play';
-clearInterval(slideInterval);
+function pauseHandler() {
+  if (!isPlaying) return;
+  isPlaying = false;
+  pauseButton.innerHTML = FA_PLAY;
+  clearInterval(timerId);
 }
 
-function playPauseSlideshow() {
-  if (isPlaying) {
-    pauseSlideshow()
-  } else {
-    playSlideshow()
-  }
-  }
+function pausePlayHandler() {
+  isPlaying ? pauseHandler() : playHandler();
+}
 
+function prevHandler() {
+  pauseHandler();
+  gotoPrev();
+}
 
-pause.addEventListener('click', playPauseSlideshow);
+function nextHandler() {
+  pauseHandler();
+  gotoNext();
+}
+
+function indicatorClickHandler(e) {
+  const { target } = e;
+
+  if (target && target.classList.contains("indicator")) {
+    const slideTo = +target.dataset.slideTo;
+    pauseHandler();
+    gotoNth(slideTo);
+  }
+}
+
+function keydownHandler(e) {
+  const { code } = e;
+  if (code === CODE_ARROW_LEFT) prevHandler();
+  if (code === CODE_ARROW_RIGHT) nextHandler();
+  if (code === CODE_SPACE) {
+    e.preventDefault();
+    pausePlayHandler();
+  }
+}
+
+function swipeStartHandler(e) {
+  swipeStartX =
+    e instanceof MouseEvent ? e.clientX : e.changedTouches[0].clientX;
+}
+
+function swipeEndHandler(e) {
+  swipeEndX = e instanceof MouseEvent ? e.clientX : e.changedTouches[0].clientX;
+
+  const diffX = swipeEndX - swipeStartX;
+  if (diffX > SWIPE_THERESHOLD) prevHandler();
+  if (diffX < -SWIPE_THERESHOLD) nextHandler();
+}
+
+function initEventListeners() {
+  pauseButton.addEventListener("click", pausePlayHandler);
+  nextButton.addEventListener("click", nextHandler);
+  prevButton.addEventListener("click", prevHandler);
+  indicatorContainer.addEventListener("click", indicatorClickHandler);
+  slidesContainer.addEventListener("touchstart", swipeStartHandler);
+  slidesContainer.addEventListener("touchend", swipeEndHandler);
+  slidesContainer.addEventListener("mousedown", swipeStartHandler);
+  slidesContainer.addEventListener("mouseup", swipeEndHandler);
+  document.addEventListener("keydown", keydownHandler);
+}
+
+function init() {
+  initEventListeners();
+  tick();
+}
+
+init();
